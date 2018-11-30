411804256be679aedac2beda35c19ef3a03fdd08
Ngoc Chien Nguyen <ngocchien.nguyen@grabtaxi.com>
2018-11-30 16:17:21 +0800

    correct error messages

--- server/handlers/push_command.go
+++ server/handlers/push_command.go
@@ -7,11 +7,8 @@ import (
 
 func (h *Handler) Push(w http.ResponseWriter, r *http.Request) {
 	code, err := h.fsm.PushCommand(r.FormValue("command"))
-	w.WriteHeader(code)
-	switch code {
-		case 400:
-		case 403:
-			fmt.Fprintf(w, "%v", err)
-		break;
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
 	}
+	w.WriteHeader(code)
 }
